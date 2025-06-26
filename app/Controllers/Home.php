9a8aab2019de1c952bee02133249d07b01f8efef
Ivaa25 <148355766+Ivaa25@users.noreply.github.com>
2025-06-26 07:19:18 -0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -9,6 +9,7 @@ use App\Models\ModelKasSosial;
 class Home extends BaseController
 {
     protected $ModelHome;
+    protected $ModelAdmin;
     protected $ModelKasMasjid;
     protected $ModelKasSosial;
 
@@ -18,38 +19,45 @@ class Home extends BaseController
         $this->ModelAdmin = new ModelAdmin();
         $this->ModelKasMasjid = new ModelKasMasjid();
         $this->ModelKasSosial = new ModelKasSosial();
-
     }
 
     public function index()
     {
         $setting = $this->ModelAdmin->ViewSetting();
+        $url = 'https://api.myquran.com/v2/sholat/jadwal/' . $setting['id_kota'] . '/' . date('Y') . '/' . date('m') . '/' . date('d');
 
-        $url = 'https://api.myquran.com/v2/sholat/jadwal/'.$setting['id_kota'].'/'.date('Y').'/'.date('m').'/'.date('d');
+        // Gunakan CURL untuk menghindari error SSL
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $waktu = json_decode(file_get_contents($url), true);
+        // HATI-HATI: Jangan gunakan ini di production kecuali sertifikat sudah valid
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        $waktu = json_decode($response, true);
 
         $data = [
-            'judul' => 'Home',
-            'page'  => 'v_home', 
-            'waktu' =>  $waktu,
-            'kas'       => $this->ModelKasMasjid->AllData(),
-            'kas_s'       => $this->ModelKasSosial->AllData(),
-
+            'judul'    => 'Home',
+            'page'     => 'v_home', 
+            'waktu'    => $waktu,
+            'kas'      => $this->ModelKasMasjid->AllData(),
+            'kas_s'    => $this->ModelKasSosial->AllData(),
         ];
 
-        return view('v_tamplate', $data); // Memanggil view template dengan data
+        return view('v_tamplate', $data);
     }
 
     public function Agenda()
     {
-        // Ambil data agenda dari model
         $data = [
-            'judul' => 'Agenda',
-            'page'  => 'front-end/v_agenda', // Pastikan file view sesuai
+            'judul'  => 'Agenda',
+            'page'   => 'front-end/v_agenda',
             'agenda' => $this->ModelHome->Agenda(),
         ];
 
-        return view('v_tamplate', $data); // Memanggil view template dengan data
+        return view('v_tamplate', $data);
     }
 }
